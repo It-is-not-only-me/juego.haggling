46e1d0fb4513914520690966f9d0099ee96f07a8
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-11-18 16:48:14 -0300

    Agregando las pruebas

---
+++ Haggling/Assets/Pruebas/Pruebas de Economia/ProductoPrueba.cs
@@ -0,0 +1,21 @@
+﻿using Economia;
+
+public partial class PruebasEconomia
+{
+    public class ProductoPrueba : Producto
+    {
+        public enum CategoriaProducto
+        {
+            Hogar, 
+            Pelea,
+            Estudio
+        }
+
+        public CategoriaProducto Categoria { get; private set; }
+
+        public ProductoPrueba(IValor valor, CategoriaProducto categoriaProducto) : base(valor)
+        {
+            Categoria = categoriaProducto;
+        }
+    }
+}
