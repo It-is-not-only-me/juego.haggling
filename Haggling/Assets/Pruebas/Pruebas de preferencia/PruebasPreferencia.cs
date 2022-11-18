b2b0e93ad0a91a1c22da4d768852bea9dba1ed54
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-11-18 16:53:38 -0300

    Preparando todo para poder hacer esta feature

---
+++ Haggling/Assets/Pruebas/Pruebas de preferencia/PruebasPreferencia.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Preferencia;
+
+public class PruebasPreferencia
+{
+    [Test]
+    public void Test01()
+    {
+ 
+    }
+}
