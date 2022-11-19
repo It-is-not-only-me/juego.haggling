15f1aadc9bf67e0ad0b54135576b6027e29182bc
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-11-18 21:11:32 -0300

    Agregando las pruebas

--- Haggling/Assets/Pruebas/Pruebas de preferencia/PruebasPreferencia.cs
+++ Haggling/Assets/Pruebas/Pruebas de preferencia/PruebasPreferencia.cs
@@ -1,15 +1,38 @@
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using Preferencia;
+using System.Net.Http.Headers;
 
-public class PruebasPreferencia
+public partial class PruebasPreferencia
 {
+
+    private const float _valorBase = 10;
+    private IValor _valorPrueba = new ValorPrueba(_valorBase);
+    private ICaracteristica _caracteristica = new CaracteristicaPrueba(CaracteristicaPrueba.Caracterizacion.Entretenimiento);
+
     [Test]
-    public void Test01()
+    public void Test01PreferenciaNoModificaElValorBaseAlNoCoincidirLaCaracteristica()
     {
- 
+        float modificador = 10;
+        ICaracteristica caracterisica = new CaracteristicaPrueba(CaracteristicaPrueba.Caracterizacion.Herramienta);
+        IPreferencia preferencia = new PreferenciaPrueba(caracterisica, modificador);
+
+        ValorPrueba valorModificado = preferencia.Valorar(_caracteristica, _valorPrueba) as ValorPrueba;
+
+        Assert.AreEqual(_valorBase, valorModificado.Valor);
+    }
+
+    [Test]
+    public void Test02PreferenciaModificaElValorBaseAlCoincidirLaCaracteristica()
+    {
+        float modificador = 10;
+        ICaracteristica caracterisica = new CaracteristicaPrueba(CaracteristicaPrueba.Caracterizacion.Entretenimiento);
+        IPreferencia preferencia = new PreferenciaPrueba(caracterisica, modificador);
+
+        ValorPrueba valorModificado = preferencia.Valorar(_caracteristica, _valorPrueba) as ValorPrueba;
+
+        Assert.AreEqual(_valorBase + modificador, valorModificado.Valor);
     }
 }
