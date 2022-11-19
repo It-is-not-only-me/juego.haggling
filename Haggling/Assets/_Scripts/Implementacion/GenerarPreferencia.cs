454854440f000bf0bf63309ab30c3f31b8c42230
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-11-19 02:04:33 -0300

    Agregando la idea de generar una preferencia
    Esto nos permite no tener que implementar el generador hasta tener lo suficiente armado para conseguirlo

---
+++ Haggling/Assets/_Scripts/Implementacion/GenerarPreferencia.cs
@@ -0,0 +1,9 @@
+using Preferencia;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGenerarPreferencia
+{
+    public IPreferencia GenerarPreferencia();
+}
